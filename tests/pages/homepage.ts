7317b1114db2c6e4a6de98acb57474423873c36d
oritro <aritra.banerjee207@gmail.com>
2024-01-06 19:00:54 +0100

    refactor homepage

--- tests/pages/homepage.ts
+++ tests/pages/homepage.ts
@@ -1,47 +1,47 @@
-import { expect, type Locator, type Page } from '@playwright/test';
-import {verifyHeading} from '../utilities/functions';
+import { type Locator, type Page } from '@playwright/test';
+import {verifyHeading, returnNumbersFromText} from '../utilities/functions';
 
 export class Homepage {
-  readonly page: Page;
-  readonly temperatureText: Locator;
-  readonly buyMoisturizersButton: Locator;
-  readonly buySunscreenButton: Locator;
+    readonly page: Page;
+    readonly temperatureText: Locator;
+    readonly buyMoisturizersButton: Locator;
+    readonly buySunscreenButton: Locator;
 
-  constructor(page: Page) {
-    this.page = page;
-    this.temperatureText = page.locator("css=#temperature");
-    this.buyMoisturizersButton = page.getByRole('button', { name: 'Buy moisturizers' });
-    this.buySunscreenButton = page.getByRole('button', { name: 'Buy sunscreens' });
-  }
-
-  async goTo(){
-    this.page.goto("/");
-  }
-
-  async getTempeture(){
-    let temperature = await this.temperatureText.innerText();
-    return Number(temperature.replace(/[^\d\-+\.]/g, ''));
-  }
-
-  async goToSuncscreenPage(){
-    await this.buySunscreenButton.click();
-    await this.page.waitForURL('**/sunscreen');
-    await verifyHeading(this.page,"Sunscreens");
-  }
-
-  async goToMoisturizersPage(){
-    await this.buyMoisturizersButton.click();
-    await this.page.waitForURL('**/moisturizer');
-    await verifyHeading(this.page,"Moisturizers");
-  }
-
-  async chooseSunscreenOrMoisturizers(){
-    let temperature = await this.getTempeture();
-    if(temperature < 19){
-        this.goToMoisturizersPage();
-    }else if(temperature > 34){
-        this.goToSuncscreenPage();
+    constructor(page: Page) {
+        this.page = page;
+        this.temperatureText = page.locator("css=#temperature");
+        this.buyMoisturizersButton = page.getByRole('button', { name: 'Buy moisturizers' });
+        this.buySunscreenButton = page.getByRole('button', { name: 'Buy sunscreens' });
     }
 
-  }
+    public async goTo(){
+        this.page.goto("/");
+    }
+
+    public async getTempeture(){
+        let temperature = await this.temperatureText.innerText();
+        return returnNumbersFromText(temperature);
+    }
+
+    public async goToSuncscreenPage(){
+        await this.buySunscreenButton.click();
+        await this.page.waitForURL('**/sunscreen');
+        await verifyHeading(this.page,"Sunscreens");
+    }
+
+    public async goToMoisturizersPage(){
+        await this.buyMoisturizersButton.click();
+        await this.page.waitForURL('**/moisturizer');
+        await verifyHeading(this.page,"Moisturizers");
+    }
+
+    public async chooseSunscreenOrMoisturizers(){
+        let temperature = await this.getTempeture();
+        if(temperature < 19){
+            this.goToMoisturizersPage();
+        }else if(temperature > 34){
+            this.goToSuncscreenPage();
+        }
+
+    }
 }
