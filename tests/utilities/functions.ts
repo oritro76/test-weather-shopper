b6a1aa70688e879462d1797ce37b68e9668b403b
oritro <aritra.banerjee207@gmail.com>
2024-01-06 19:04:22 +0100

    implement utility function

--- tests/utilities/functions.ts
+++ tests/utilities/functions.ts
@@ -4,3 +4,10 @@ export async function verifyHeading(page:Page, headingText:string) {
     let heading = await page.getByRole('heading').innerText();
     expect(heading).toBe(headingText);
 }
+
+export function returnNumbersFromText(text:string) {
+    const numberPattern = /\d+(\.\d+)?/; // Regular expression to match numbers (integer or floating-point)
+    const match = text.match(numberPattern);
+    return Number(match[0]);
+       
+}
